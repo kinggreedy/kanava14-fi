8c934437648f2abb280971480957442f3253f89a
Chau Nguyen <chau.nguyen.x64@gmail.com>
2020-09-25 18:58:01 +0300

    Initial commit

---
+++ scripts/vagrant/sync-app.ps1
@@ -0,0 +1,9 @@
+$project = "blog-platform";
+$server = "192.168.102.199";
+$username = "vagrant";
+$password = "vagrant";
+$port = "10422";
+$project_host_dir = "../../app/$project";
+$project_guest_dir = "ssh://$server//opt/python3test/$project";
+
+unison -fat -perms 0 -dontchmod -watch -repeat watch -prefer $project_guest_dir -ignore "Path .pytest_cache" -ignore "Path log" -sshargs "-l $username -P $port -pw $password" $project_host_dir $project_guest_dir
